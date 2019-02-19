31b427a898abc1170c76bba5e4066f3c02ff3101
Distiller <distiller@static.162.221.90.99.cyberlynk.net>
2019-02-19 13:59:13 -0800

    eu - Eucalypt (Haskell Impl: v0.1.1.1610)

--- Formula/eucalypt.rb
+++ Formula/eucalypt.rb
@@ -1,20 +1,21 @@
-class Eucalypt < Formula
 
-  version "0.1.1.1534"
+class Eucalypt < Formula
+  version "0.1.1.1610"
   desc "Eucalypt (eu) tool for generating, templating and processing YAML, JSON, TOML"
   homepage "https://curvelogic.github.io/eucalypt/"
-  url "https://github.com/curvelogic/eucalypt/releases/download/0.1.1.1534/eucalypt-x86_64-osx.tgz"
-  sha256 "80e69682dd2ced5882d8669de237b01aa46d3ac52927dd48c383ebac75adfe97"
+  url "https://github.com/curvelogic/eucalypt/releases/download/0.1.1.1610/eucalypt-x86_64-osx.tgz"
+  sha256 "55a2b967838f38d76ee6542697cc18f878a70246ac674eaed262803c0fadf1f0"
   bottle :unneeded
-
+  
   def install
     bin.install "eu"
   end
-
-
+  
+  
   def test
-    expected = "Eucalypt (Haskell Impl: v#{version})"
+    expected = "Eucalypt (Haskell Impl: v#{package.version})"
     assert_equal expected, `#{bin}/eu --version`.strip
   end
-
+  
 end
+
