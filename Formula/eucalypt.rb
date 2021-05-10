7dd93d84f85827256789114223e5ef81ab925a86
gmorpheme <github@gmorpheme.net>
2021-05-10 12:24:33 +0100

    Release eucalypt 0.2.0.40

--- Formula/eucalypt.rb
+++ Formula/eucalypt.rb
@@ -1,10 +1,10 @@
 
 class Eucalypt < Formula
-  version "0.1.1.2042"
+  version "0.2.0.40"
   desc "Eucalypt (eu) tool for generating, templating and processing YAML, JSON, TOML"
   homepage "https://curvelogic.github.io/eucalypt/"
-  url "https://github.com/curvelogic/eucalypt/releases/download/0.1.2.46/eucalypt-x86_64-osx.tgz"
-  sha256 "e57a23a9381111d6e0c8919acf1165bd6dcdc9f48a428bffbbfd26ff43e3f88b"
+  url "https://github.com/curvelogic/eucalypt/releases/download/0.2.0.40/eucalypt-x86_64-osx.tgz"
+  sha256 "74c4ebb6b4c47468038d3d9bcee4ec9b73e8ca59ce181dd2ed8884fb7f5c68bf"
   bottle :unneeded
   
   def install
@@ -13,7 +13,7 @@ class Eucalypt < Formula
   
   
   def test
-    expected = "Eucalypt (Haskell Impl: v#{package.version})"
+    expected = "Eucalypt (Rust Impl: v#{package.version})"
     assert_equal expected, `#{bin}/eu --version`.strip
   end
   
