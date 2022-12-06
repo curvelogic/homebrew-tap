4816a2794afe94b8264ec7070244652efa52e086
gmorpheme <github@gmorpheme.net>
2022-12-06 13:29:39 +0000

    Release eucalypt 0.2.0.224

--- Formula/eucalypt.rb
+++ Formula/eucalypt.rb
@@ -2,8 +2,8 @@ class Eucalypt < Formula
   version "0.2.0.202"
   desc "Eucalypt (eu) tool for generating, templating and processing YAML, JSON, TOML"
   homepage "https://curvelogic.github.io/eucalypt/"
-  url "https://github.com/curvelogic/eucalypt/releases/download/0.2.0.202/eucalypt-x86_64-osx.tgz"
-  sha256 "bd275f9f9af81058f8b140b38f45c07635fdffdb56c6f36f43643a11c5600772"
+  url "https://github.com/curvelogic/eucalypt/releases/download/0.2.0.224/eucalypt-x86_64-osx.tgz"
+  sha256 "69716d57cfb19668ca7a5331ecbf470320d1b3e7982bad10b397f2f61c4b92e6"
 
   def install
     bin.install "eu"
