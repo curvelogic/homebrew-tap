6f4402661d0f17d8fb45deea8e1fe8d873b1c71f
Greg Hawkins <162815+gmorpheme@users.noreply.github.com>
2026-02-08 12:01:25 +0000

    Release eucalypt 0.3.0.361

--- Formula/eucalypt.rb
+++ Formula/eucalypt.rb
@@ -1,16 +1,21 @@
+
 class Eucalypt < Formula
-  desc "Tool for generating, templating and processing YAML, JSON, TOML"
+  version "0.3.0.361"
+  desc "Eucalypt (eu) tool for generating, templating and processing YAML, JSON, TOML"
   homepage "https://curvelogic.github.io/eucalypt/"
-  url "https://github.com/curvelogic/eucalypt/releases/download/0.2.0.352/eucalypt-x86_64-osx.tgz"
-  version "0.2.0.352"
-  sha256 "45dada59bf6be761f8b5ef7c4ec0b2c5f02da80a0119a75b3aea8c67c1af8787"
+  url "https://github.com/curvelogic/eucalypt/releases/download/0.3.0.361/eucalypt-x86_64-osx.tgz"
+  sha256 "f752f3645f4f61de030c93a9064ffbd05738359d6e92101e1ec1392276beeef7"
+  bottle :unneeded
 
   def install
     bin.install "eu"
   end
 
-  test do
-    expected = "eu - Eucalypt (Rust Impl: v#{package.version})"
+
+  def test
+    expected = "Eucalypt (Rust Impl: v#{package.version})"
     assert_equal expected, `#{bin}/eu --version`.strip
   end
+
 end
+
