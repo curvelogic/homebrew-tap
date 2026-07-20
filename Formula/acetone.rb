2c3a0e723df7f8bde8d378b9a004da68a192260a
Greg Hawkins <162815+gmorpheme@users.noreply.github.com>
2026-07-20 17:47:35 +0100

    acetone 0.2.0

--- Formula/acetone.rb
+++ Formula/acetone.rb
@@ -1,26 +1,26 @@
 class Acetone < Formula
   desc "Version-controlled labelled property graph database, queried with openCypher"
   homepage "https://github.com/curvelogic/acetone"
-  version "0.1.1"
+  version "0.2.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/curvelogic/acetone/releases/download/v0.1.1/acetone-v0.1.1-aarch64-apple-darwin.tar.gz"
-      sha256 "1d711ce263e21570530b1b631250e87cc737498ef8b81b6a5aa156425737ec6e"
+      url "https://github.com/curvelogic/acetone/releases/download/v0.2.0/acetone-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "c706793d84afab10ca1cf0fc3237f3b4179a21c63b60d52d2ac1a67d82cbb1aa"
     else
-      url "https://github.com/curvelogic/acetone/releases/download/v0.1.1/acetone-v0.1.1-x86_64-apple-darwin.tar.gz"
-      sha256 "0c96d16aeeb97d8446e3585857b9a7898902baf199ed6ea914b97ca769696580"
+      url "https://github.com/curvelogic/acetone/releases/download/v0.2.0/acetone-v0.2.0-x86_64-apple-darwin.tar.gz"
+      sha256 "227916a39673025dfab1514f7a38189fd4fd481e905f9d58065d7587f76b6954"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/curvelogic/acetone/releases/download/v0.1.1/acetone-v0.1.1-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "777041cc46340e9bde4ae7505c3f49e2155ca1fb86dd162ca89a3c400eb5da86"
+      url "https://github.com/curvelogic/acetone/releases/download/v0.2.0/acetone-v0.2.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "d5d6010ad76a66e8d30f8654fae48584c0ecb7f4669666264b896d76671865c7"
     else
-      url "https://github.com/curvelogic/acetone/releases/download/v0.1.1/acetone-v0.1.1-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "1f553eb47555b24014ace78d68c4678fe9a0c261a5b6afc60d1d556ed90ce8e0"
+      url "https://github.com/curvelogic/acetone/releases/download/v0.2.0/acetone-v0.2.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "80a36942c8e0c3080b4663cada11216ec8b0e946e268281ea457ffd222b3f2d1"
     end
   end
 
